1b8058cf7d9d3c93bfcdf86898f1503ee3c5f5e9
Bryan Kuntz <kuntz.bryan@gmail.com>
2015-07-23 14:43:41 -0400

    prepare for communication

--- Robot/Robot.ino
+++ Robot/Robot.ino
@@ -3,15 +3,66 @@
 #define solenoidPin 7
 #define motorPin 3
 
+bool solenoid = false;
+bool buttonPressed = false;
+
 void setup()
 {
 	pinMode(solenoidPin, OUTPUT);
 	pinMode(motorPin, OUTPUT);
+
+	digitalWrite(solenoidPin, HIGH);
 
 	Serial.begin(BAUD);
 }
 
 void loop()
 {
+	if(!solenoid && buttonPressed)
+	{
+		digitalWrite(solenoidPin, LOW);
+		delay(200);
+		digitalWrite(solenoidPin, HIGH);
+		solenoid = true;
+	}
+	else if(solenoid && !buttonPressed)
+	{
+		solenoid = false;
+	}
+}
 
+void SerialEvent()
+{
+	byte input = Serial.read();
+
+	if((input & serialHeader) == serialHeader)
+	{
+		byte data = input & serialHeader;
+
+		if((data & buttonBit) == buttonBit)
+		{
+			Serial.println("Button Pressed");
+			buttonPressed = true;
+		}
+
+		if((data & leftBit) == leftBit)
+		{
+			Serial.println("Left Pressed");
+		}
+
+		if((data & rightBit) == rightBit)
+		{
+			Serial.println("Right Pressed");
+		}
+
+		if((data & upBit) == upBit)
+		{
+			Serial.println("Up Pressed");
+		}
+
+		if((data & downBit) == downBit)
+		{
+			Serial.println("Down Pressed");
+		}
+	}
 }
