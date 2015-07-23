94b09d79a12f4b3b4eea2992175393bdbd005092
Austin <snider.austin5@gmail.com>
2015-07-23 16:12:21 -0400

    Fix lights

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -1,16 +1,28 @@
 #include "../Flags.h"
 
+//#define DEBUG
+
 #define buttonPin 2
 #define xPin A3
 #define yPin A1
+#define greenPin 13
+#define yellowPin 7
 
 int count = 0;
+
+bool lightOn = false;
 
 void setup()
 {
 	pinMode(buttonPin, INPUT_PULLUP);
 	pinMode(xPin, INPUT);
 	pinMode(yPin, INPUT);
+
+	pinMode(greenPin, OUTPUT);
+	pinMode(yellowPin, OUTPUT);
+
+	digitalWrite(yellowPin, HIGH);
+
 	Serial.begin(BAUD);
 }
 
@@ -28,6 +40,8 @@ void loop()
 
 	if(count % 20 == 0)
 	{
+		bool changed = false;
+
 		int xIn = analogRead(xPin);
 		int yIn = analogRead(yPin);
 
@@ -37,24 +51,43 @@ void loop()
 		if(xIn > 70)
 		{
 			serialOut |= rightBit;
+			changed = true;
 		}
 		else if(xIn < 40)
 		{
 			serialOut |= leftBit;
+			changed = true;
 		}
 
 		if(yIn > 65)
 		{
 			serialOut |= upBit;
+			changed = true;
 		}
 		else if(yIn < 30)
 		{
 			serialOut |= downBit;
+			changed = true;
+
 		}
 		count = 0;
 
+		#ifdef DEBUG
+		Serial.println(serialOut);
+		#else
 		Serial.write(serialOut);
+		#endif
 
+		lightOn = changed;
+	}
+
+	if(lightOn)
+	{
+		digitalWrite(greenPin, HIGH);
+	}
+	else
+	{
+		digitalWrite(greenPin, LOW);
 	}
 	
 	count++;
